f6c43d7056fad136b72ac93f6a2c2eb0720f3a57
gbxu@Cathy <gb.xu@outlook.com>
2018-05-02 14:04:24 +0800

    add the tools of test_shell

---
+++ tools/test_shell/test_yours.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd ..
+#python ../../tools/launch.py  -s 1 -n 1 --launcher ssh -H hosts --brokers node14:9092,node15:9092,node16:9092 --sync-dst-dir /home/xugb/ic_kafka_test/  python train_mnist.py --network lenet --kv-store dist_sync
+echo "hello world"
